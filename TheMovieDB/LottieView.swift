b3dfedf14ada58d64f7a0616c8457fa148b7bd2d
Jangkung <jangkungarim@gmail.com>
2020-02-09 19:56:41 +0700

    first commit

---
+++ TheMovieDB/LottieView.swift
@@ -0,0 +1,21 @@
+//
+//  LottieView.swift
+//  TheMovieDB
+//
+//  Created by Jangkung Ari on 09/02/20.
+//  Copyright © 2020 Jangkung Ari. All rights reserved.
+//
+
+import SwiftUI
+
+struct LottieView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LottieView_Previews: PreviewProvider {
+    static var previews: some View {
+        LottieView()
+    }
+}
